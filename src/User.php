748d1f42c8c5eb562be51166b4cecce20f69110c
vuviy <vova.banudz@gmail.com>
2026-05-02 10:26:46 +0300

    feat(user): add age field

--- src/User.php
+++ src/User.php
@@ -4,11 +4,13 @@ class User
 {
     private string $name;
     private string $email;
+    private int $age;
 
-    public function __construct(string $name, string $email)
+    public function __construct(string $name, string $email, int $age)
     {
         $this->name  = $name;
         $this->email = $email;
+        $this->age   = $age;
     }
 
     public function getName(): string
@@ -19,6 +21,10 @@ class User
     public function getEmail(): string
     {
         return $this->email;
+    }
+    public function getAge(): int
+    {
+        return $this->age;
     }
 
     public function isValidEmail(): bool

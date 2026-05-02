22db4942c89de3d3844db4e22c4f514a78118ac3
vuviy <vova.banudz@gmail.com>
2026-05-02 13:14:42 +0300

    feat(logger): add Logger class with info/warning/error levels
    Implements basic file logger with timestamp support.
    
    Closes #1

---
+++ src/Logger.php
@@ -0,0 +1,34 @@
+<?php
+
+class Logger
+{
+    private string $logFile;
+
+    public function __construct(string $logFile = 'app.log')
+    {
+        $this->logFile = $logFile;
+    }
+
+    public function info(string $message): void
+    {
+        $this->write('INFO', $message);
+    }
+
+    public function warning(string $message): void
+    {
+        $this->write('WARNING', $message);
+    }
+
+    public function error(string $message): void
+    {
+        $this->write('ERROR', $message);
+    }
+
+    private function write(string $level, string $message): void
+    {
+        $timestamp = date('Y-m-d H:i:s');
+        $line      = "[$timestamp] [$level] $message" . PHP_EOL;
+
+        file_put_contents($this->logFile, $line, FILE_APPEND);
+    }
+}
